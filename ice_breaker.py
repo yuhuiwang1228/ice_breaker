e6ce8d149d7379f78a417d3314acb0befeb536e6
yuhuiwang1228 <1670594368@qq.com>
2024-05-06 13:30:22 -0400

    update branch

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,6 +7,7 @@ from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.twitter import scrape_user_tweets
+from output_parser import summary_parser
 
 
 def ice_break_with(name: str) -> str:
@@ -23,18 +24,23 @@ def ice_break_with(name: str) -> str:
     2. two interesting facts about them 
 
     Use both information from twitter and Linkedin
+    \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information", "twitter_posts"], template=summary_template
+        input_variables=["information", "twitter_posts"], 
+        template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-
+    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm |summary_parser
     res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
-    print(res)
+    # print(res)
+    print(res.summary)
+    print(res.facts)
 
 
 if __name__ == "__main__":
--- third_parties/twitter.py
+++ third_parties/twitter.py
@@ -15,7 +15,7 @@ twitter_client = tweepy.Client(
 )
 
 
-def scrape_user_tweets(username, num_tweets=5, mock: bool = False):
+def scrape_user_tweets(username, num_tweets=5, mock: bool = True):
     """
     Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of dictionaries.
     Each dictionary has three fields: "time_posted" (relative to now), "text", and "url".
