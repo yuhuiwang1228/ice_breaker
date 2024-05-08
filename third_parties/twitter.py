1366a0ef56bb8586d55abb4680d70e9b11217e79
yuhuiwang1228 <1670594368@qq.com>
2024-05-08 01:00:42 -0400

    update branch

--- third_parties/twitter.py
+++ third_parties/twitter.py
@@ -18,7 +18,7 @@ twitter_client = tweepy.Client(
 
 
 
-def scrape_user_tweets(username, num_tweets=5, mock: bool = True):
+def scrape_user_tweets(username, num_tweets=5, mock: bool = False):
     """
     Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of dictionaries.
     Each dictionary has three fields: "time_posted" (relative to now), "text", and "url".
@@ -47,4 +47,4 @@ def scrape_user_tweets(username, num_tweets=5, mock: bool = True):
 
 if __name__ == "__main__":
 
-    print(scraper_user_tweets(username="EdenEmarco177"))
+    print(scrape_user_tweets(username="EdenEmarco177"))
